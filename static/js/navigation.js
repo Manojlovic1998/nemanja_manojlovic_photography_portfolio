e11a91e10ccd0b8c515018f000d374efbe1b615a
Nemanja Manojlovic <manojlovic911@hotmail.com>
2022-02-11 21:05:47 +0100

    Prevent body scroll on mob nav bar open

--- static/js/navigation.js
+++ static/js/navigation.js
@@ -1,22 +1,23 @@
 let navOpenStatus = false;
 
-
 window.addEventListener("load", function () {
-    let buttonNode = document.getElementById("nav-btn");
-    let sideNavNode = document.getElementById("side-nav");
+  let buttonNode = document.getElementById("nav-btn");
+  let sideNavNode = document.getElementById("side-nav");
+  let bodyNode = document.getElementById("body");
 
-    buttonNode.addEventListener("click", onBtnClickHandler);
+  buttonNode.addEventListener("click", onBtnClickHandler);
 
-
-    function onBtnClickHandler() {
-        if (navOpenStatus) {
-            buttonNode.classList.remove("open");
-            sideNavNode.classList.add("d-none");
-            navOpenStatus = !navOpenStatus;
-        } else {
-            buttonNode.classList.add("open");
-            sideNavNode.classList.remove("d-none");
-            navOpenStatus = !navOpenStatus;
-        }
+  function onBtnClickHandler() {
+    if (navOpenStatus) {
+      bodyNode.classList.remove("prevent-scroll");
+      buttonNode.classList.remove("open");
+      sideNavNode.classList.add("d-none");
+      navOpenStatus = !navOpenStatus;
+    } else {
+      bodyNode.classList.add("prevent-scroll");
+      buttonNode.classList.add("open");
+      sideNavNode.classList.remove("d-none");
+      navOpenStatus = !navOpenStatus;
     }
+  }
 });
